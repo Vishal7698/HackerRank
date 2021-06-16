2b83251d08dbe35fa6b966c5129f313895629f01
VISHAL HASE <vishalhase1111@gmail.com>
2021-06-16 22:47:30 +0530

    1st day of 30 day of code

---
+++ 30 days of code/1.Data_Type.py
@@ -0,0 +1,37 @@
+"""Input Format
+
+The first line contains an integer that you must sum with i.
+The second line contains a double that you must sum with d.
+The third line contains a string that you must concatenate with s.
+
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1=int(input())
+d1=float(input())
+s1=str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+i1)
+print(d+d1)
+print(s+s1)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
